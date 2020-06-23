32527e740e01f8253617bde14525f69d8a5fb50d
kalyankalapala <kalyankalapala24@gmail.com>
2020-06-23 13:17:07 +0000

    db-2

--- accounts.sql
+++ accounts.sql
@@ -29,7 +29,7 @@ CREATE TABLE `MyGuests` (
   `email` varchar(50) DEFAULT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `MyGuests` (
 
 LOCK TABLES `MyGuests` WRITE;
 /*!40000 ALTER TABLE `MyGuests` DISABLE KEYS */;
-INSERT INTO `MyGuests` VALUES (1,'kalyan','kalapala','kalyankalapala@gmail.com','1996-04-05 00:00:00'),(2,'harish','m','harishm@gmail.com','1995-04-05 00:00:00'),(3,'sachin','t','sachin@gmail.com','1992-04-05 00:00:00'),(6,'dhoni','ms','dhoni@gmail.com','1990-09-05 00:00:00');
+INSERT INTO `MyGuests` VALUES (1,'kalyan','kalapala','kalyankalapala@gmail.com','1996-04-05 00:00:00'),(2,'harish','m','harishm@gmail.com','1995-04-05 00:00:00'),(3,'sachin','t','sachin@gmail.com','1992-04-05 00:00:00'),(6,'dhoni','ms','dhoni@gmail.com','1990-09-05 00:00:00'),(7,'kohli','v','kohli@gmail.com','1991-01-05 00:00:00');
 /*!40000 ALTER TABLE `MyGuests` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-23 13:12:44
+-- Dump completed on 2020-06-23 13:16:20
